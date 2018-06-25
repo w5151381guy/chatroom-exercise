2c91afaa73f243ef6d41bc3cba30d4af654e940b
Andy Chen <w5151381guy@users.noreply.github.com>
2018-06-25 10:38:05 +0800

    Update config.js

--- final/frontend/src/utils/config.js
+++ final/frontend/src/utils/config.js
@@ -1,4 +1,2 @@
 export const URL_WEBSOCKET = `ws://localhost:8000/ws/chat/`
-export const APIKEY = 'AIzaSyDxNYEN1cKv6csxcbgNZQBQvJg3FW33HJk'
-export const CLIENTID =
-  '208097841984-rgrs57cqikl88m6pct4co4h1fu99nqic.apps.googleusercontent.com'
+export const CLIENTID = '208097841984-rgrs57cqikl88m6pct4co4h1fu99nqic.apps.googleusercontent.com'
