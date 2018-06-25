bb4d5e37da8fdc9a7b4a32be4d59e7e7417a9b1b
Andy Chen <w5151381guy@users.noreply.github.com>
2018-06-25 11:36:46 +0800

    Update config.js

--- final/frontend/src/utils/config.js
+++ final/frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-export const URL_WEBSOCKET = `ws://localhost:8000/ws/chat/`
+export const URL_WEBSOCKET = `ws://w5151381guy.ddns.net:8000/ws/chat/`
 export const CLIENTID = '208097841984-rgrs57cqikl88m6pct4co4h1fu99nqic.apps.googleusercontent.com'
